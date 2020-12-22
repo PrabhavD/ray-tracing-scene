66d4a41e8d717b2ae99361c67356b5992b541f2f
Prabhav Desai <63824619+PrabhavD@users.noreply.github.com>
2020-12-21 22:12:02 -0700

    comments

--- vec3.h
+++ vec3.h
@@ -8,8 +8,8 @@ using std::sqrt;
 
 class vec3 {
     public:
-        vec3() : e{0,0,0} {}
-        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+        vec3() : e{0,0,0} {}    //constructor initializes to origin marked as 0,0,0
+        vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}    //overloading for input coordinates if given
 
         double x() const { return e[0]; }
         double y() const { return e[1]; }
