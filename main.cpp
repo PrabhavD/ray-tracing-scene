890f6c177faf00afbca91667bc6bbb4ec21232bf
Prabhav Desai <63824619+PrabhavD@users.noreply.github.com>
2020-12-28 21:51:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,19 @@
 #include "sphere.h"
 
 #include <iostream>
-color ray_color(const ray& r, const hittable& world) {
+
+color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return color(0,0,0);
+
     if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
     }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
